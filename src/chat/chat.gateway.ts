b2d558d5485360bf2f0c8703ebd80b86db1cff31
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2019-12-16 20:08:50 +0100

    first working chat

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -5,30 +5,39 @@ import { Socket, Server } from 'socket.io';
 @WebSocketGateway(4444) // WebSocket port number: 4444
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
-	private logger: Logger = new Logger('AppGetway');
-
+	private logger: Logger = new Logger('ChatGetway');
+	private mainClient: Socket;
+	private cliensList: Socket[] = [];
 	afterInit(server: Server) {
-		this.logger.log('init');
+		this.logger.log('ChatGetway init');
 	}
 
 	handleConnection(client: Socket, ...args: any[]) {
 		this.logger.log(`Client connected: ${client.id}`);
+		this.cliensList.push(client);
 	}
 	handleDisconnect(client: Socket) {
+		this.cliensList = this.cliensList.filter( fclient => fclient.id !== client.id);
 		this.logger.log(`Client disconnected: ${client.id}`);
 	}
 
 	@SubscribeMessage('messageFromMainClientToServer')
-	handleMessageFromMainClient(client: Socket, text: string): WsResponse<string> {
-		this.logger.log(`from main client: ${client.id}`);
-		this.logger.log(`message recieved: ${text}`);
-		return { event: 'messageFromServerToMainClient', data: 'mssg recieved!' };
+	handleMessageFromMainClient(client: Socket, data: string): WsResponse<string> | void {
+			this.logger.log(this.cliensList.length);
+			this.mainClient = client;
+			// const target = this.cliensList.find( fclient => fclient.id === data.to );
+			const target = this.cliensList[0];
+			this.logger.log(target.id);
+			if (target) {
+				this.logger.log(target.emit('messageFromMainClientToClient', data));
+			}
+			// return { event: 'messageFromServerToMainClient', data: 'MainClienFirstHandshake' };
+
 	}
 
 	@SubscribeMessage('messageFromClientToServer')
 	handleMessageFromClient(client: Socket, text: string): WsResponse<string> {
-		this.logger.log(`from client: ${client.id}`);
-		this.logger.log(`message recieved: ${text}`);
-		return { event: 'messageFromServerToClient', data: text };
+		this.mainClient.emit('messageFromClientToMainClient', text);
+		return { event: 'messageFromServerToClient', data: 'recieved in server' };
 	}
 }

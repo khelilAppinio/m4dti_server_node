0b4d13fc6bcf7bb486eb9d3ffa2208b73e8ddc5d
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2020-01-18 20:13:20 +0100

    add some validation

--- src/dtos/create-message-dto.ts
+++ src/dtos/create-message-dto.ts
@@ -1,6 +1,11 @@
+import { IsString, IsBoolean } from 'class-validator';
 export class CreateMessageDto {
+	@IsBoolean()
 	readonly isAdmin: boolean;
+	@IsString()
 	readonly body: string;
+	@IsString()
 	readonly date: string; // TODO: change to switable type.
+	@IsString()
 	readonly username: string; // TODO change to uniqueId
 }

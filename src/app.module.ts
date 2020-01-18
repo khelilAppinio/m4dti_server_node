3ef17135a2a2d20e56f9d7b174e99c1864548bf4
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2020-01-18 16:18:37 +0100

    add ConnectedClientsHistoryModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat/chat.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConnectedClientsHistoryModule } from './modules/connected-clients-history/connected-clients-history.module';
+import { ConnectedClientsHistoryService } from './services/connected-clients-history/connected-clients-history.service';
 
 @Module({
-	imports: [MongooseModule.forRoot('mongodb://localhost/nestFirst')],
+	imports: [
+		MongooseModule.forRoot('mongodb://localhost/nestFirst'),
+		ConnectedClientsHistoryModule,
+	],
 	controllers: [],
 	providers: [ChatGateway],
 })

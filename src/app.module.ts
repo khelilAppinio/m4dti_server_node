9fe5f426cf40e95fbc0048803b87efbf9037251d
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2020-01-18 18:40:06 +0100

    add message module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat/chat.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConnectedClientsHistoryModule } from './modules/connected-clients-history/connected-clients-history.module';
+import { MessageModule } from './modules/message/message.module';
 
 @Module({
 	imports: [
 		MongooseModule.forRoot('mongodb://localhost/nestFirst'),
 		ConnectedClientsHistoryModule,
+		MessageModule,
 	],
 	controllers: [],
 	providers: [ChatGateway],

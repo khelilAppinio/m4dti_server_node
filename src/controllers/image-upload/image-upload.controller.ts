1a13de762872d3c471fe6a878022f1628c193e09
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2020-01-22 12:07:45 +0100

    add messageService and chatGateway dependencies

--- src/controllers/image-upload/image-upload.controller.ts
+++ src/controllers/image-upload/image-upload.controller.ts
@@ -2,20 +2,34 @@ import { Controller, Post, Body, Res, HttpStatus, Logger, Get } from '@nestjs/co
 import * as fs from 'fs';
 import * as path from 'path';
 import { Response } from 'express';
-
+import * as uuid from 'uuid/v4';
+import { MessageService } from '../../services/message/message.service';
+import { ChatGateway } from '../../chat/chat.gateway';
 @Controller('image-upload')
 export class ImageUploadController {
+
+	constructor(private readonly messageService: MessageService, private readonly chatGateway: ChatGateway) {}
 	@Post()
-	writeImage(@Body('data') data: string, @Res() res: Response) {
+	writeImage(@Body('data') data: string, @Body('sourceSocketId') sourceSocketId: string, @Res() res: Response) {
+		// ! TODO: use regex to filter png|jpeg|jpg ..
+		// ! TODO: save in the db as message
+		// ! TODO: get sourceSocketId from client
+		// ! TODO: validate data
+		// ! TODO: add remove image feature for both front and back
+		// ! TODO: send media url to the destination
+		const imgId = uuid();
+		const date = new Date().getTime();
 		fs.writeFile(
-			path.join(__dirname, '../../../public/uploaded_images/1234.jpeg'),
+			path.join(__dirname, `../../../public/uploaded_images/${imgId}.jpeg`),
 			data.replace(/^data:image\/jpeg;base64,/, ''),
 			'base64', (error) => {
 				if (error) {
 					Logger.error(error);
 					return res.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).send();
 				}
-				return res.status(HttpStatus.OK).send();
+				// send recieved media from a client to the main client
+				this.chatGateway.sendMedia(imgId, sourceSocketId, date);
+				return res.status(HttpStatus.OK).send({imgId});
 		});
 	}
 }

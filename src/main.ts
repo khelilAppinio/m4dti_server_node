b490d70ae0bad4f243402dca8b352ef2d58cb9e7
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2020-01-23 16:57:05 +0100

    add cors and request entity limit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	// ! TODO: cors need to be removed
+	const app = await NestFactory.create(AppModule, { cors: true });
+	// ! TODO: limit need to be removed
+	app.use(bodyParser.json({ limit: '50mb' }));
+	app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 	await app.listen(3000);
 }
 bootstrap();

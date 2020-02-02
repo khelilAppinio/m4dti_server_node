2ef1c2e16ada3b26d4ea0346fca65580d01cf14e
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2020-02-02 20:40:36 +0100

    add salt 2

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,6 +3,7 @@ import * as mongoose from 'mongoose';
 export const UserSchema = new mongoose.Schema({
 	username: {type: String, unique: true},
 	password: String,
+	salt: String,
 	avatar: String,
 	status: String,
 });

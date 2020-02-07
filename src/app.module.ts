764862fa120d131ff35fe9383cef10fe4bba3e51
khelilAppinio <khelil.amari@appinio.com>
2020-02-07 15:41:58 +0000

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot('mongodb://localhost/nestFirst'),
+		MongooseModule.forRoot('mongodb://mongo:27017/nestFirst'),
 		ConnectedClientsHistoryModule,
 		MessageModule,
 		AuthModule,

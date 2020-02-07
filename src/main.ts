bf025fea388cc1f076c2ec9c68a193c131340f3f
khelilAppinio <khelil.amari@appinio.com>
2020-02-07 17:21:38 +0000

    hotfix: https issue

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as fs from 'fs';
 async function bootstrap() {
 	const httpsOptions = {
-		key: fs.readFileSync(join(__dirname, '..', 'cert/server.key')),
-		cert: fs.readFileSync(join(__dirname, '..', 'cert/server.cert')),
+		key: fs.readFileSync(join(__dirname, '..', 'cert/privkey.pem')),
+		cert: fs.readFileSync(join(__dirname, '..', 'cert/cert.pem')),
 	};
 	// ! TODO: cors need to be removed
 	const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true, httpsOptions });
 	app.useStaticAssets(join(__dirname, '..', 'public/uploaded_images'), { prefix: '/public/uploaded_images' });
 	app.useStaticAssets(join(__dirname, '..', 'public/uploaded_audios'), { prefix: '/public/uploaded_audios' });
-	app.useStaticAssets(join(__dirname, '..', '.well-known/acme-challenge'), { prefix: '/.well-known/acme-challenge' });
 
 	// ! TODO: limit need to be removed
 	app.use(bodyParser.json({ limit: '50mb' }));
